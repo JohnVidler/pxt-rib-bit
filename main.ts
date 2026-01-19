bfa9cb6197cbf649ebd4e0eecc23e71a17d0fca0
Dr John Vidler <j.vidler@lancaster.ac.uk>
2026-01-19 11:47:45 +0000

    Scaffolding the basic LoRa blocks to match the regular radio blocks

--- main.ts
+++ main.ts
@@ -19,13 +19,70 @@ namespace RibBitCamera {
     }
 }
 
-//% block="Rib:Bit Radio"
+//% block="Rib:Bit LoRa Radio"
 //% color="#3ecb21"
 //% icon="\u26A0"
-//% groups=[]
+//% groups=[ "Group", "Send", "Recieve" ]
 namespace RibBitRadio {
     //% block="switch LoRa radio $state"
-    export function switchLoRa(state: RibBit.OnOff = RibBit.OnOff.On): void {
+    export function switchLoRa(state: RibBit.OnOff = 1): void {
+        return;
+    }
+
+    //% block="set LoRa group to $group"
+    export function setRadioGroup( group: number = 0 ): void {
+        return;
+    }
+
+    //% block="LoRa group"
+    export function getRadioGroup(): number {
+        return 0;
+    }
+
+    //% block "set LoRa power to $power"
+    //% power.min=0
+    //% power.max=10
+    //% group="Send"
+    export function setRadioPower( power: number = 5 ): void {
+        return;
+    }
+
+    //% block="LoRa send number $value"
+    //% group="Send"
+    export function sendNumber( value: number = 0 ): void {
+        return
+    }
+
+    //% block="LoRa send value $field = $value"
+    //% group="Send"
+    export function sendValue( field: string = "name", value: any = 0 ): void {
+        return
+    }
+
+    //% block="LoRa send string $value"
+    //% group="Send"
+    export function sendString( value: string = "" ): void {
+        return
+    }
+
+    //% block="on LoRa number received"
+    //% draggableParameters="inline"
+    //% group="Receive"
+    export function onNumberReceived( cb: (value: number) => {} ): void {
+        return
+    }
+
+    //% block="on LoRa value received"
+    //% draggableParameters="inline"
+    //% group="Receive"
+    export function onValueReceived(cb: (field: string, value: any) => {}): void {
+        return
+    }
+
+    //% block="on LoRa string received"
+    //% draggableParameters="inline"
+    //% group="Receive"
+    export function onStringReceived(cb: (value: string) => {}): void {
         return
     }
 }
@@ -33,15 +90,20 @@ namespace RibBitRadio {
 //% block="Rib:Bit GPS"
 //% color="#3ecb21"
 //% icon="\u26A0"
-//% groups=["Time Functions","Spatial Functions"]
+//% groups=[ "Time Functions","Spatial Functions" ]
 namespace RibBitGPS {
     //% block="switch gps $state"
     export function switchGPS(state: RibBit.OnOff = RibBit.OnOff.On): void {
         return
     }
 
+    //% block="is GPS on?"
+    //% advanced="true"
+    export function isGPSOn(): boolean {
+        return false;
+    }
+
     //% block="on a position update"
-    //% draggableParameters="reporter"
     //% group="Spatial Functions"
     export function on2DPositionUpdate( cb: () => {} ): void {
         return
@@ -71,10 +133,22 @@ namespace RibBitGPS {
         return 0;
     }
 
+    //% block="on a time update"
+    //% group="Time Functions"
+    export function onTimeUpdate( cb: () => {} ): void {
+        return
+    }
+
     //% block="the current time"
     //% group="Time Functions"
     export function getTime(): string {
         return "hh:mm:ss";
+    }
+
+    //% block="the current date"
+    //% group="Time Functions"
+    export function getDate(): string {
+        return "YYYY:MM:DD";
     }
 
     //% block="the hour"
