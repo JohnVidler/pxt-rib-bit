eec9798ec194044371a520381aad46f4df330404
Dr John Vidler <j.vidler@lancaster.ac.uk>
2026-03-19 12:14:14 +0000

    Update hooks.cpp

--- hooks.cpp
+++ hooks.cpp
@@ -7,7 +7,10 @@ namespace RibBit {
     //%
     bool isUsbPowered() {
         #if MICROBIT_CODAL
-            static NRF52Pin vusbPin(DEVICE_ID_IO_P0 + 34, 34, PIN_CAPABILITY_DIGITAL);
+            #ifndef MICROBIT_PIN_VUSB
+                #define MICROBIT_PIN_VUSB 34
+            #endif
+            static NRF52Pin vusbPin(ID_PIN_VUSB, MICROBIT_PIN_VUSB, PIN_CAPABILITY_DIGITAL);
             return vusbPin.getDigitalValue() == 1;
         #else
             return false;
