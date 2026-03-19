d99bd0e3bb07ed6e704ce0e39d8804d5c1fa2334
Dr John Vidler <j.vidler@lancaster.ac.uk>
2026-03-19 12:09:02 +0000

    Patched out MICROBIT_PIN_VUSB dependency

--- hooks.cpp
+++ hooks.cpp
@@ -7,7 +7,7 @@ namespace RibBit {
     //%
     bool isUsbPowered() {
         #if MICROBIT_CODAL
-            static NRF52Pin vusbPin(MICROBIT_PIN_VUSB);
+            static NRF52Pin vusbPin(DEVICE_ID_IO_P0 + 34, 34, PIN_CAPABILITY_DIGITAL);
             return vusbPin.getDigitalValue() == 1;
         #else
             return false;
